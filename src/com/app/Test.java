636126a792af4661578f9636697b5e8d20e67287
SunilKumar <SunilKumar@SunilKumar-PC>
2017-05-16 16:54:13 +0530

    test class modified

--- src/com/app/Test.java
+++ src/com/app/Test.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 public static void main(String[] args) {
+	@SuppressWarnings("resource")
 	ApplicationContext c=new ClassPathXmlApplicationContext("config.xml");
 	Employee e=(Employee)c.getBean("empObj");
 	e.getMsg();
